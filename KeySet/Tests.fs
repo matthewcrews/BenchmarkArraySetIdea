de6882cb1bc0930bc302145ad6ef6aceb3666d20
Matthew Crews <matthewcrews@gmail.com>
2020-08-18 05:47:18 -0700

    Updating tests

--- KeySet/Tests.fs
+++ KeySet/Tests.fs
@@ -20,15 +20,15 @@ let rng = System.Random(123)
 //    |> Array.map KeySet
 
 
-let numberOfSets = 100_000
-let populationSize = 100
-let setSize = 30
-let numberIterations = numberOfSets
+let numberOfSets = 10_000
+let populationSize = 50
+let setSize = 10
+let numberIterations = 100_000
 
 let setData =
     let rng = System.Random(123)
     [|for _ in 0..numberOfSets-1 do
-        [|for _ in 1..setSize -> (rng.Next(0, populationSize))|]
+        [|for _ in 1..setSize ->  (rng.Next(0, populationSize))|]
         |> Set.ofArray
     |]
 let keySetData = setData |> Array.map KeySet
